af25bece92133fe6469daefc47201dbb2d8527fc
tylerjmalloy <tylerjmalloy@gmail.com>
2023-10-13 17:40:07 -0400

    adding simulations

--- data/colorsa/draw.py
+++ data/colorsa/draw.py
@@ -1,23 +1,37 @@
 import cairo
 
-surface = cairo.ImageSurface(cairo.FORMAT_RGB24, 64, 64)
-ctx = cairo.Context(surface)
+paths = ["raw/orange.png", "raw/green.png", "raw/purple.png"]
+for idx, colors in enumerate([[0.75, 0.5, 0.0], [0.5, 0.75, 0], [0.5, 0, 0.75]]):
+    path = paths[idx]
+    # Create a PNG surface and a context
+    width, height = 64, 64
+    surface = cairo.ImageSurface(cairo.FORMAT_RGB24, width, height)
+    context = cairo.Context(surface)
 
-ctx.rectangle(0, 0, 64, 64)
-ctx.set_source_rgb(0.75, 0.5, 0) # orange
-ctx.fill()
-surface.write_to_png('raw/orange.png')
+    context.set_source_rgb(1,1,1) # transparent white
+    context.rectangle(0, 0, 64, 64)
+    context.fill()
 
-ctx = cairo.Context(surface)
+    # Set the fill color to the color
+    #context.set_source_rgb(colors[0], colors[1], colors[2])  # RGB values for the color
 
-ctx.rectangle(0, 0, 64, 64)
-ctx.set_source_rgb(0.5, 0.75, 0) # purple
-ctx.fill()
-surface.write_to_png('raw/green.png')
+    # Draw a circle in the center of the surface
+    center_x, center_y = width / 2, height / 2
+    radius = min(center_x, center_y) - 1  # 10-pixel padding
+    border_width = 3
 
-ctx = cairo.Context(surface)
+    # Draw the circle border (black)
+    context.set_source_rgb(0.0, 0.0, 0.0)  # RGB values for black
+    context.set_line_width(border_width)
+    context.arc(center_x, center_y, radius, 0, 2 * 3.141592)  # 2 * π
+    context.stroke()
 
-ctx.rectangle(0, 0, 64, 64)
-ctx.set_source_rgb(0.5, 0, 0.75) # green
-ctx.fill()
-surface.write_to_png('raw/purple.png')
+    # Fill the circle with the color
+    context.set_source_rgb(colors[0], colors[1], colors[2])  # RGB values for the color
+    context.arc(center_x, center_y, radius - border_width + 2, 0, 2 * 3.141592)  # 2 * π
+    context.fill()
+
+    # Save the result to a PNG file
+    surface.write_to_png(path)
+
+
--- disvae/models/losses.py
+++ disvae/models/losses.py
@@ -14,9 +14,9 @@ from disvae.utils.math import (log_density_gaussian, log_importance_weight_matri
                                matrix_log_density_gaussian)
 
 
-UTIL_LOSSES = ["mse", "L1"]
-LOSSES = ["VAE", "betaH", "betaB", "factor", "btcvae"]
-RECON_DIST = ["bernoulli", "laplace", "gaussian"]
+UTIL_LOSSES = ["mse"]
+LOSSES = ["VAE", "betaH"]
+RECON_DIST = ["gaussian"]
 
 
 # TO-DO: clean n_data and device
@@ -29,26 +29,6 @@ def get_loss_f(loss_name, **kwargs_parse):
         return BetaHLoss(beta=kwargs_parse["betaH_B"], upsilon=kwargs_parse["upsilon"], **kwargs_all)
     elif loss_name == "VAE":
         return BetaHLoss(beta=1, upsilon=kwargs_parse["upsilon"], **kwargs_all)
-    elif loss_name == "betaB":
-        return BetaBLoss(C_init=kwargs_parse["betaB_initC"],
-                         C_fin=kwargs_parse["betaB_finC"],
-                         gamma=kwargs_parse["betaB_G"],
-                         upsilon=kwargs_parse["upsilon"],
-                         **kwargs_all)
-    elif loss_name == "factor":
-        return FactorKLoss(kwargs_parse["device"],
-                           gamma=kwargs_parse["factor_G"],
-                           disc_kwargs=dict(latent_dim=kwargs_parse["latent_dim"]),
-                           optim_kwargs=dict(lr=kwargs_parse["lr_disc"], betas=(0.5, 0.9)),
-                           upsilon=kwargs_parse["upsilon"],
-                           **kwargs_all)
-    elif loss_name == "btcvae":
-        return BtcvaeLoss(kwargs_parse["n_data"],
-                          alpha=kwargs_parse["btcvae_A"],
-                          beta=kwargs_parse["btcvae_B"],
-                          gamma=kwargs_parse["btcvae_G"],
-                          upsilon=kwargs_parse["upsilon"],
-                          **kwargs_all)
     else:
         assert loss_name not in LOSSES
         raise ValueError("Uknown loss : {}".format(loss_name))
@@ -165,249 +145,6 @@ class BetaHLoss(BaseLoss):
 
         return loss
 
-
-class BetaBLoss(BaseLoss):
-    """
-    Compute the Beta-VAE loss as in [1]
-
-    Parameters
-    ----------
-    C_init : float, optional
-        Starting annealed capacity C.
-
-    C_fin : float, optional
-        Final annealed capacity C.
-
-    gamma : float, optional
-        Weight of the KL divergence term.
-
-    kwargs:
-        Additional arguments for `BaseLoss`, e.g. rec_dist`.
-
-    References
-    ----------
-        [1] Burgess, Christopher P., et al. "Understanding disentangling in
-        $\beta$-VAE." arXiv preprint arXiv:1804.03599 (2018).
-    """
-
-    def __init__(self, C_init=0., C_fin=20., gamma=100., upsilon=1, **kwargs):
-        super().__init__(**kwargs)
-        self.gamma = gamma
-        self.C_init = C_init
-        self.C_fin = C_fin
-        self.upsilon=upsilon
-
-    def __call__(self, data, recon_data, utilities, recon_utilities, latent_dist, is_train, storer, **kwargs):
-        storer = self._pre_call(is_train, storer)
-
-        util_loss = _utility_loss(utilities, recon_utilities, util_loss=self.util_loss)
-        rec_loss = _reconstruction_loss(data, recon_data,
-                                        storer=storer,
-                                        distribution=self.rec_dist)
-        kl_loss = _kl_normal_loss(*latent_dist, storer)
-
-        C = (linear_annealing(self.C_init, self.C_fin, self.n_train_steps, self.steps_anneal)
-             if is_train else self.C_fin)
-
-        loss = rec_loss + (self.upsilon * util_loss) + self.gamma * (kl_loss - C).abs()
-
-        if storer is not None:
-            storer['loss'].append(loss.item())
-
-        return loss
-
-
-class FactorKLoss(BaseLoss):
-    """
-    Compute the Factor-VAE loss as per Algorithm 2 of [1]
-
-    Parameters
-    ----------
-    device : torch.device
-
-    gamma : float, optional
-        Weight of the TC loss term. `gamma` in the paper.
-
-    discriminator : disvae.discriminator.Discriminator
-
-    optimizer_d : torch.optim
-
-    kwargs:
-        Additional arguments for `BaseLoss`, e.g. rec_dist`.
-
-    References
-    ----------
-        [1] Kim, Hyunjik, and Andriy Mnih. "Disentangling by factorising."
-        arXiv preprint arXiv:1802.05983 (2018).
-    """
-
-    def __init__(self, device,
-                 gamma=10.,
-                 disc_kwargs={},
-                 optim_kwargs=dict(lr=5e-5, betas=(0.5, 0.9)),
-                 **kwargs):
-        super().__init__(**kwargs)
-        self.gamma = gamma
-        self.device = device
-        self.discriminator = Discriminator(**disc_kwargs).to(self.device)
-        self.optimizer_d = optim.Adam(self.discriminator.parameters(), **optim_kwargs)
-
-    def __call__(self, *args, **kwargs):
-        raise ValueError("Use `call_optimize` to also train the discriminator")
-
-    def call_optimize(self, data, utilities, recon_utilities, model, optimizer, storer):
-        storer = self._pre_call(model.training, storer)
-
-        # factor-vae split data into two batches. In the paper they sample 2 batches
-        batch_size = data.size(dim=0)
-        half_batch_size = batch_size // 2
-        data = data.split(half_batch_size)
-        data1 = data[0]
-        data2 = data[1]
-
-        # Factor VAE Loss
-        recon_batch, latent_dist, latent_sample1 = model(data1)
-        rec_loss = _reconstruction_loss(data1, recon_batch,
-                                        storer=storer,
-                                        distribution=self.rec_dist)
-
-        kl_loss = _kl_normal_loss(*latent_dist, storer)
-
-        util_loss = _utility_loss(utilities, recon_utilities, self.util_loss)
-
-        d_z = self.discriminator(latent_sample1)
-        # We want log(p_true/p_false). If not using logisitc regression but softmax
-        # then p_true = exp(logit_true) / Z; p_false = exp(logit_false) / Z
-        # so log(p_true/p_false) = logit_true - logit_false
-        tc_loss = (d_z[:, 0] - d_z[:, 1]).mean()
-        # with sigmoid (not good results) should be `tc_loss = (2 * d_z.flatten()).mean()`
-
-        anneal_reg = (linear_annealing(0, 1, self.n_train_steps, self.steps_anneal)
-                      if model.training else 1)
-        vae_loss = rec_loss + (self.upsilon * util_loss) + kl_loss + anneal_reg * self.gamma * tc_loss
-
-        if storer is not None:
-            storer['loss'].append(vae_loss.item())
-            storer['tc_loss'].append(tc_loss.item())
-
-        if not model.training:
-            # don't backprop if evaluating
-            return vae_loss
-
-        # Compute VAE gradients
-        optimizer.zero_grad()
-        vae_loss.backward(retain_graph=True)
-
-        # Discriminator Loss
-        # Get second sample of latent distribution
-        latent_sample2 = model.sample_latent(data2)
-        z_perm = _permute_dims(latent_sample2).detach()
-        d_z_perm = self.discriminator(z_perm)
-
-        # Calculate total correlation loss
-        # for cross entropy the target is the index => need to be long and says
-        # that it's first output for d_z and second for perm
-        ones = torch.ones(half_batch_size, dtype=torch.long, device=self.device)
-        zeros = torch.zeros_like(ones)
-        d_tc_loss = 0.5 * (F.cross_entropy(d_z, zeros) + F.cross_entropy(d_z_perm, ones))
-        # with sigmoid would be :
-        # d_tc_loss = 0.5 * (self.bce(d_z.flatten(), ones) + self.bce(d_z_perm.flatten(), 1 - ones))
-
-        # TO-DO: check ifshould also anneals discriminator if not becomes too good ???
-        #d_tc_loss = anneal_reg * d_tc_loss
-
-        # Compute discriminator gradients
-        self.optimizer_d.zero_grad()
-        d_tc_loss.backward()
-
-        # Update at the end (since pytorch 1.5. complains if update before)
-        optimizer.step()
-        self.optimizer_d.step()
-
-        if storer is not None:
-            storer['discrim_loss'].append(d_tc_loss.item())
-
-        return vae_loss
-
-
-class BtcvaeLoss(BaseLoss):
-    """
-    Compute the decomposed KL loss with either minibatch weighted sampling or
-    minibatch stratified sampling according to [1]
-
-    Parameters
-    ----------
-    n_data: int
-        Number of data in the training set
-
-    alpha : float
-        Weight of the mutual information term.
-
-    beta : float
-        Weight of the total correlation term.
-
-    gamma : float
-        Weight of the dimension-wise KL term.
-
-    is_mss : bool
-        Whether to use minibatch stratified sampling instead of minibatch
-        weighted sampling.
-
-    kwargs:
-        Additional arguments for `BaseLoss`, e.g. rec_dist`.
-
-    References
-    ----------
-       [1] Chen, Tian Qi, et al. "Isolating sources of disentanglement in variational
-       autoencoders." Advances in Neural Information Processing Systems. 2018.
-    """
-
-    def __init__(self, n_data, alpha=1., beta=6., gamma=1., is_mss=True, **kwargs):
-        super().__init__(**kwargs)
-        self.n_data = n_data
-        self.beta = beta
-        self.alpha = alpha
-        self.gamma = gamma
-        self.is_mss = is_mss  # minibatch stratified sampling
-
-    def __call__(self, data, recon_batch, utilities, recon_utilities, latent_dist, is_train, storer,
-                 latent_sample=None):
-        storer = self._pre_call(is_train, storer)
-        batch_size, latent_dim = latent_sample.shape
-
-        rec_loss = _reconstruction_loss(data, recon_batch,
-                                        storer=storer,
-                                        distribution=self.rec_dist)
-        log_pz, log_qz, log_prod_qzi, log_q_zCx = _get_log_pz_qz_prodzi_qzCx(latent_sample,
-                                                                             latent_dist,
-                                                                             self.n_data,
-                                                                             is_mss=self.is_mss)
-        # I[z;x] = KL[q(z,x)||q(x)q(z)] = E_x[KL[q(z|x)||q(z)]]
-        mi_loss = (log_q_zCx - log_qz).mean()
-        # TC[z] = KL[q(z)||\prod_i z_i]
-        tc_loss = (log_qz - log_prod_qzi).mean()
-        # dw_kl_loss is KL[q(z)||p(z)] instead of usual KL[q(z|x)||p(z))]
-        dw_kl_loss = (log_prod_qzi - log_pz).mean()
-
-        anneal_reg = (linear_annealing(0, 1, self.n_train_steps, self.steps_anneal)
-                      if is_train else 1)
-        
-        util_loss = _utility_loss(utilities, recon_utilities, self.util_loss)
-
-        # total loss
-        loss = rec_loss + (self.upsilon * util_loss) + (self.alpha * mi_loss +
-                           self.beta * tc_loss +
-                           anneal_reg * self.gamma * dw_kl_loss)
-
-        if storer is not None:
-            storer['loss'].append(loss.item())
-            storer['mi_loss'].append(mi_loss.item())
-            storer['tc_loss'].append(tc_loss.item())
-            storer['dw_kl_loss'].append(dw_kl_loss.item())
-            # computing this for storing and comparaison purposes
-            _ = _kl_normal_loss(*latent_dist, storer)
-
-        return loss
 
 def _utility_loss(utilities, recon_utilities, util_loss="mse", storer=None):
     if (utilities is None or recon_utilities is None):
@@ -554,8 +291,6 @@ def linear_annealing(init, fin, step, annealing_steps):
     return annealed
 
 
-# Batch TC specific
-# TO-DO: test if mss is better!
 def _get_log_pz_qz_prodzi_qzCx(latent_sample, latent_dist, n_data, is_mss=True):
     batch_size, hidden_dim = latent_sample.shape
 
